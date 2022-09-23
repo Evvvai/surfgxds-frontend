a1ffb4fbd8be41eca921cf01f3b1a5da9c33ed72
Ivan Kornilov <ivan.kornilov@nordclan.com>
2022-09-23 21:34:22 +0300

    fix

--- components/layouts/Main.layout.tsx
+++ components/layouts/Main.layout.tsx
@@ -1,4 +1,4 @@
-import { FC, Fragment } from 'react'
+import { FC, Fragment, PropsWithChildren } from 'react'
 
 // Styles
 import styles from '../../styles/layouts/Layouts.module.scss'
@@ -14,7 +14,7 @@ import MenuAside from '../menu-aside/MenuAside.component'
 // Utils
 
 //////////////////////////////////////////////////////////////////////////////////////
-const MainLayout: FC = ({ children }) => {
+const MainLayout = ({ children }: PropsWithChildren) => {
   return (
     <Fragment>
       <MenuAside />
